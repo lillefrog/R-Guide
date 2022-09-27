379329c642b7141f6a51d4e29c283455abe4db0d
lillefrog <chBrandt@health.sdu.dk>
2022-09-27 13:21:35 +0200

    Demonstration of exponential distributions from a youtube dataset

--- textbook figures/CH01/ExponentialDistEx.R
+++ textbook figures/CH01/ExponentialDistEx.R
@@ -1,68 +1,51 @@
 # exponential distribution 
+
+# Load my preferred colors
 MyColores = c("#115f9a", "#1984c5", "#22a7f0", "#48b5c4", "#76c68f", "#a6d75b", "#c9e52f", "#d0ee11", "#d0f400")
 
-
-set.seed(51) # 
-x <- rexp(10000, rate=0.00048)
-
-blocks <- seq(0, 20000, by=1000) # how do we split the data
-
-#hist(x)
-
-hist(x, breaks = blocks,
-     xlab="Number of views",
-     ylab="Number of videos",
-     main="Youtube videos",
-     xlim = c(0,20000),
-     ylim = c(0,4000),
-     col = MyColores[2])
-
-
-xx = x[x>10000]
-hist(xx, breaks = blocks,
-     xlab="Number of views",
-     ylab="Number of videos",
-     ylim = c(0,30),
-     main="Youtube videos",
-     col = MyColores[2])
-
-# load youtube dataset ----
-
+# use the readr libary to read data from a text file that contains data from youtube
 library(readr)
-rawdata <- read_delim("~/doc1/OUH/E07 Videnskabsteori og Statestik/Datasets/Youtube Dataset/youtube_0305u.txt", 
+rawData <- read_delim("textbook figures/datasets/Youtube Dataset/youtube_0305u.txt", 
                       delim = "\t", 
                       escape_double = FALSE, 
                       trim_ws = TRUE)
 
+# extract the number of views for all videos
+views <- na.omit(rawData$VIEWS)
 
-views = na.omit(rawData$VIEWS)
+# Set the margins and that we want to combine figures in 1 column and 3 rows 
+par(mar=c(5,5,5,2), mfrow=c(1,3))
 
-blocks <- seq(0, 20000, by=1000) # how do we split the data
-
-
-viv2 = viv[viv<10000]
+# plot videos with less than 10,000 views in the first figure
+viv2 = views[views<10000]
 hist(viv2,
      xlab="Number of views",
      ylab="Number of videos",
-     main="Youtube videos from 0 to 10,000 views",
-     col = MyColores[2])
+     main="from 0 to 10,000 views",
+     col = MyColores[2]) # use my default color number2
 
-viv3 = viv[viv>10000 & viv<100000]
+# plot videos with 10,000 to 100,000 views in the second figure
+viv3 = views[views>10000 & views<100000]
 hist(viv3,
      xlab="Number of views (x1000)",
      ylab="Number of videos",
-     main="Youtube videos from 10,000 to 100,000 views",
-     xaxt='n',
+     main="from 10,000 to 100,000 views",
+     xaxt='n', # remove the original x-axis
      col = MyColores[2])
-axis(side=1,at=seq(10000,100000,10000),labels=seq(10,100,10))
+# Because we want the axis to show numbers divided with 1000 we have to define our own axis
+axis(side=1,at=seq(10000,100000,10000),labels=seq(10,100,10)) 
 
-viv4 = viv[viv>100000 & viv<1000000]
+# plot videos with 100,000 to 1,000,000 views in the third figure
+viv4 = views[views>100000 & views<1000000]
 hist(viv4,
      xlab="Number of views (x1000)",
      ylab="Number of videos",
-     main="Youtube videos from 100,000 to 1000,000 views",
-     xaxt='n',
+     main="from 100,000 to 1,000,000 views",
+     xaxt='n', # remove the original x-axis
      col = MyColores[2])
-axis(side=1,at=seq(100000,1000000,100000),labels=seq(100,1000,100))
-
+# Because we want the axis to show numbers divided with 1000 we have to define our own axis
+axis(side=1,  # side 1 is the x-axis
+     at=seq(100000,1000000,100000), # at is where the text should be placed
+     labels=seq(100,1000,100)
+     )
 
