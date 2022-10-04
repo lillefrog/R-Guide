09c73e34ce7bfcc3d534b6b3c7ca318d46242785
lillefrog <chBrandt@health.sdu.dk>
2022-10-04 11:06:50 +0200

    added simple pie chart

---
+++ Simple figures/PieChart.R
@@ -0,0 +1,18 @@
+# simple pie chart
+
+# here I define a set of colors I like to use If you just want to use the default 
+# colors you can remove the col = MyColoes line from the plot
+MyColoes = c("#1984c5", "#22a7f0", "#48b5c4", "#76c68f", "#a6d75b", "#c9e52f", "#d0ee11")
+
+
+# make some data
+slices <- c(11747, 6194, 4289, 3675) # size of each slice
+navn <- c("Tea", "Chocolate", "clothes", "Shoes") # name of each slice
+
+# Simple Pie Chart
+pie(slices, 
+    labels = navn,     # name / lables of the slices
+    col = MyColoes,    # set the colors of the slices, this line can be removed
+    main="My budget for 2022", # main title of the plot
+    cex = 1.1          # set the size of the text 1= normal
+    )
