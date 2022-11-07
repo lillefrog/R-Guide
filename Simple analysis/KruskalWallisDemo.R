322fa8e1eb8b2df6f0acb90e57999b0f4fced1b1
lillefrog <chBrandt@health.sdu.dk>
2022-11-07 08:43:22 +0100

    Updated Kruskal wallis to use intern dataset and include more explanation

--- Simple analysis/KruskalWallisDemo.R
+++ Simple analysis/KruskalWallisDemo.R
@@ -1,31 +1,56 @@
 # Kruskal-Wallis Demo
+# This is the non parametric alternative to ANOVA
 
-install.packages("palmerpenguins")
-install.packages("FSA")
-library(palmerpenguins)
 
-penguinData <- penguins[, c("species", "flipper_length_mm")] # make a new dataset with only species and flipper length
+## Dataset ----
+# This is the same dataset as used for the ANOVA example
+G1	<- c(1604,	1422,	1337,	1311,	1147,	1159,	1047,	1381,	1276,	1582,	1686,	1922,	2192,	2020,	2053,	2144)
+G2	<- c(639,	566,	568,	568,	514,	543,	490,	503,	508,	686,	767,	761,	968,	969,	785,	972)
+G3	<- c(1071,	960,	1026,	1038,	956,	1013,	833,	843,	807,	1061,	1171,	1225,	1304,	1282,	1215,	1491)
+G4	<- c(840,	905,	901,	901,	921,	875,	764,	769,	834,	952,	1227,	1212,	1295,	1306,	1126,	1149)
+G5	<- c(399,	384,	354,	382,	331,	419,	340,	317,	357,	366,	469,	436,	432,	493,	345,	534)
+groupData <- data.frame(G1, G2, G3, G4, G5)
 
-summary(penguinData)
+# convert data to long format as with the ANOVA
+LongData <- stack(groupData,       # this only works if the data is in vectors, if your data is another type you might have to convert
+                  select = -c(G5)  # This deselects one or more collums (notice the - sign) use it to deselect data that cant be used
+)
 
-boxplot(flipper_length_mm ~ species ,data = penguinData)
 
-kruskal.test(flipper_length_mm ~ species,
-             data = penguinData)
+## Kruskal-Wallis test ----
+# let us have a look at the data and see the collum names
+summary(LongData)
+
+# make a boxplot, this is not required but nice to have
+boxplot(values ~ ind ,data = LongData)
+
+# make the actual test
+kruskal.test(values ~ ind,
+             data = LongData)
+
+# the result should show a significant effect p= 4.0 e-9 so the groups are not equal
 
 # If we want to see what groups are different from the others we have to make 
 # a post-hoc test.This only makes sense if we already got a significant result
 # in the Kruskal-Wallis tests.
 
+
+## post-hoc test ----
 # The Dunn test is th most common post-hoc test when doing Kruskal-Wallis. But we
 # need a separate library for that
 
+
+# This install has to be run the first time you use the dunn test
+#install.packages("FSA")
 library(FSA)
 
-dunnTest(flipper_length_mm ~ species,
-         data = penguinData,
+dunnTest(values ~ ind,
+         data = LongData,
          method = "holm"
          )
+
+# only the difference between G3 and G4 is not significant (p=0.372) This fits well
+# with what we saw in the boxplot
 
 # notice that you should use the adjusted p values (p.adj) to avoid problems 
 # with multiple tests
