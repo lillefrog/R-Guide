8ceb86119c0e7799ad196e44344d64033e63d2c5
Lillefrog <Christian_Brandt@yahoo.com>
2024-03-04 07:07:44 +0100

    update to use a fixed seed

--- textbook figures/CH01/PoissonDistributionDemo.R
+++ textbook figures/CH01/PoissonDistributionDemo.R
@@ -1,11 +1,15 @@
 # classical demo of poisson distribution
 # the number of Prussian soldiers accidentally killed by horse-kick
+png(file = "02_possion_dist_demo.png", units="in", width=7, height=4, res=300)
 
+set.seed(4)
 x <- rpois(200, lambda=0.61)
 hist(x, 
      breaks = 0:7-0.5,
      col = 'blue',                     # Color of bars
      border = 'white',                  # color of borders around bars
-     main='Death by horse in the army over 20 years',
+     main='Deaths by horse',
      ylab='Number of events',
      xlab ='Deaths pr year')     
+
+dev.off() # enable this if you want to save a png file (first line also has to be active)
